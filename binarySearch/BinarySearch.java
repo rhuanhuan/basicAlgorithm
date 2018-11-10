dba6707fd8d8981e27f63d44cc53f01032f97b34
rhuanhuan <1210098674@qq.com>
2018-11-10 18:05:27 +0800

    put order outside of the binary search

--- binarySearch/BinarySearch.java
+++ binarySearch/BinarySearch.java
@@ -1,22 +1,19 @@
-import java.util.Arrays;
-
 /*
-1. Sort the source int list in ascending order;
+1. The source int list should be in ascending order;
 2. compare the mid value with toBeFoundValue;
 3. change the [start, end] scope, until find the final result
 4. return -1 if not find the result
 */
 
 public class BinarySearch {
-  public static int findKey(int toBeFoundValue, int[] sourceIntList){
-    Arrays.sort(sourceIntList);
+  public static int findKey(int toBeFoundValue, int[] ascendingIntList){
     int start = 0;
-    int end = sourceIntList.length - 1;
+    int end = ascendingIntList.length - 1;
     while (start <= end){
       int mid = start + (end - start)/2;
-      if (sourceIntList[mid] > toBeFoundValue)
+      if (ascendingIntList[mid] > toBeFoundValue)
         end = mid - 1;
-      else if (sourceIntList[mid] < toBeFoundValue)
+      else if (ascendingIntList[mid] < toBeFoundValue)
         start = mid + 1;
       else
         return mid;
--- binarySearch/BinarySearchTest.java
+++ binarySearch/BinarySearchTest.java
@@ -1,8 +1,11 @@
+import java.util.Arrays;
+
 public class BinarySearchTest {
   private static int[] initArray = {1, 2, 4, 7, 12, 461, 562, 123, 256, 10};
   private static int toBeFoundValue = 123;
 
   public static void main(String[] args){
+    Arrays.sort(initArray);
     int result = BinarySearch.findKey(toBeFoundValue, initArray);
     if (result == -1)
       System.out.println("Result not found");
